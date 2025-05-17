66d7fa5633e7833a0a0f04e9c18bf5f534c2f05c
rusjaell <rusjaellgo@gmail.com>
2025-05-17 14:20:47 +0100

    Experiment with Vue

--- assets/graphics_engine/glfw_window_creation.cpp
+++
@@ -1,6 +0,0 @@
-GLFWwindow* window = glfwCreateWindow(1280, 720, "Graphics Engine", nullptr, nullptr);
-if (window == nullptr)
-{
-    std::cout << "Failed to Create Window" << std::endl;
-    return -1;
-}
